8ffc10343733cf2d0b064e3191df941a439bcfad
Hemanathan <arulmugilan@gmail.com>
2021-03-23 16:09:54 +0530

    mini assign2
    made changes so player can run on both directions horizontally and flipping works
    added debug code and tested it

--- Hames 2d/Assets/Scripts/PlayerController.cs
+++ Hames 2d/Assets/Scripts/PlayerController.cs
@@ -4,12 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Animator animator;
     private void Awake()
     {
         Debug.Log("Player controller awake");
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    /*    private void OnCollisionEnter2D(Collision2D collision)
+        {
+            Debug.Log("Collision: " + collision.gameObject.name);
+        }*/
+
+    private void Update()
     {
-        Debug.Log("Collision: " + collision.gameObject.name);
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        Debug.Log(horizontal);
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+
+        Vector3 scale = transform.localScale;
+        if (horizontal < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if (horizontal > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+
+        //Jump
+        Input.GetAxisRaw("Vertical");
+        Input.GetKeyDown(KeyCode.Space);
     }
 }
